5b685ee666b3d9e1c5efedd661f89fbc3ccedf27
Tomáš Mozdřeň <46648569+BeanGreen247@users.noreply.github.com>
2019-10-11 11:56:43 +0200

    Update bot.js

--- js/bot.js
+++ js/bot.js
@@ -111,8 +111,8 @@ function answer(x) {
                     window.open('https://www.instagram.com', '_blank');
                 return;
                 }else if (question.slice(0,10).includes("!OPENSITE") ) {
-                    botSays("\nBot : I´ll open Instagram for you " + you); botChat.scrollTop = botChat.scrollHeight;
-                    window.open(''+x.slice(8), '_blank');
+                    botSays("\nBot : I´ll open "+x.slice(8)+"  for you " + you); botChat.scrollTop = botChat.scrollHeight;
+                    window.open('x.slice(8)', '_blank');
                 return;
                 } else if (question.slice(0,4).includes("!ME") ) {
                     youDo(x.slice(4)); botChat.scrollTop = botChat.scrollHeight;
